8dadf9a7be738e375b008274d385a34ace8a3432
Tiiwoo <tiiwoo.taowei@outlook.com>
2024-05-06 19:02:50 +0800

    feat: add game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(5).map((index) => (
-        <span key={index} class="cell">
-          {value ? value[index] : undefined}
-        </span>
+        <Cell
+          key={index}
+          letter={result ? result[index].letter : undefined}
+          status={result ? result[index].status : undefined}
+        />
       ))}
     </p>
   );

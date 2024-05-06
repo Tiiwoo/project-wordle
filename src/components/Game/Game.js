20407d607cb750dd8ecb9511865598dfed5e182b
Tiiwoo <tiiwoo.taowei@outlook.com>
2024-05-06 17:37:40 +0800

    feat: keeping track of guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
 import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,9 +12,16 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGusses] = React.useState([]);
+
+  function handleSubmitGuess(newGuess) {
+    setGusses([...guesses, newGuess]);
+  }
+
   return (
     <>
-      <GuessInput />
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
 }

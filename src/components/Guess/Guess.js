1629573f90cd8f83d8f9a837bfbc081a184103b7
Tiiwoo <tiiwoo.taowei@outlook.com>
2024-05-06 18:03:54 +0800

    feat: guess slots

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { range } from '../../utils';
+
+function Guess({ value }) {
+  return (
+    <p className="guess">
+      {range(5).map((index) => (
+        <span key={index} class="cell">
+          {value ? value[index] : undefined}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
